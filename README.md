dc5643aaa0105ca2136f2ed57fbd9b2f60e54fdf
Ruinan Zhang <zhangr4@rose-hulman.edu>
2016-10-16 01:06:09 -0400

    add choices and default plot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Biplot for Smari
+### Description
+    This is a statistical project to conduct a Principal Component Analysis by implementing a biplot for company Smari.  
---
+++ shinny_myggbiplot/server.R
@@ -0,0 +1,48 @@
+library(shiny)
+require(openxlsx)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(ggbiplot)
+library(HDclassif)
+source("myggbiplot.R")
+
+
+shinyServer(function(input,output){
+  
+  output$myggbiplot <- reactivePlot(function(){
+    
+    
+    if (input$vecs==""){
+      selected =  NULL
+    }else{
+      selected = input$vecs
+    }
+    inputAlpha = as.numeric(input$alpha)
+    inputPercentage = as.numeric(input$percentage)
+    inputChoices = as.numeric(input$choices)
+    print(inputChoices)
+    # use different df based on user input or default input
+    if(!is.null(input$file)){
+            df <- read.xlsx(input$file$datapath,sheet = 1)
+            df = df[complete.cases(df),]
+            df = df[,sapply(df, function(v) var(v, na.rm=TRUE)!=0)]   # remove constant column
+            df_pca <- prcomp(df,center = TRUE,scale. = TRUE)
+            g <- myggbiplot(df_pca,sample_ratio=inputPercentage,select_features=selected,alpha = input$alpha,
+                            choices = inputChoices)
+          }else{
+            df = data(wine)
+            df_pca = prcomp(wine,center = TRUE,scale. = TRUE)
+            g <- myggbiplot(df_pca,sample_ratio = inputPercentage,select_features = selected, alpha= input$alpha,
+                            groups = as.factor(wine$class), ellipse = TRUE,choices = inputChoices)
+          }
+    print(g)
+    }
+  )
+  
+  output$ui <- renderUI({
+    
+  })
+
+  
+})
