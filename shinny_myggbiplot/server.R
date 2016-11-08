9f6b1de47ed3ed13e0852004555d887a70293090
Ruinan Zhang <zhangr4@rose-hulman.edu>
2016-11-08 12:28:53 -0500

    add more features

--- shinny_myggbiplot/server.R
+++ shinny_myggbiplot/server.R
@@ -25,24 +25,39 @@ shinyServer(function(input,output){
             g <- myggbiplot(df_pca,sample_ratio=inputPercentage,select_features=selected,alpha = input$alpha,
                             choices = inputChoices)
           }else{
-            df = data(wine)
-            df_pca = prcomp(wine,center = TRUE,scale. = TRUE)
+            df = (wine)
+            if(!is.null(input$features_for_pca)){
+              df = df[,colnames(df) %in% (input$features_for_pca)]
+            }
+            df_pca = prcomp(df,center = TRUE,scale. = TRUE)
             g <- myggbiplot(df_pca,sample_ratio = inputPercentage,select_features = selected, alpha= input$alpha,
-                            groups = as.factor(wine$class), ellipse = TRUE,choices = inputChoices)
+                            groups = as.factor(wine$class), ellipse = TRUE,choices = inputChoices)+ggtitle(input$plot_title)
           }
     print(g)
     }
   )
   
   output$cols <- renderUI({
+#     if(!is.null(input$file)){
+#       df <- read.xlsx(input$file$datapath,sheet = 1)
+#       col_names <- names(df)
+#     }else{
+#       data(wine)
+#       col_names <- names(wine)
+#     }
+    col_names = input$features_for_pca
+    checkboxGroupInput("cols",label = h4("Pick features to display"),col_names,inline = TRUE,selected = col_names)
+  })
+  
+  output$features <- renderUI({
     if(!is.null(input$file)){
       df <- read.xlsx(input$file$datapath,sheet = 1)
       col_names <- names(df)
     }else{
-      data(wine)
-      col_names <- names(wine)
+      df = (wine)
+      col_names <- names(df)
+      checkboxGroupInput("features_for_pca",label = h4("Pick features for PCA analysis"),col_names,inline = TRUE,selected = col_names)
     }
-    checkboxGroupInput("cols",label = h4("Pick Features"),col_names,inline = TRUE)
   })
   
   
--- shinny_myggbiplot/ui.R
+++ shinny_myggbiplot/ui.R
@@ -12,16 +12,19 @@ shinyUI(fluidPage(
      fileInput("file", label = h4("Import Excel File"),accept = c(".xlsx",".XLSX")),
      selectInput("percentage",label = "Ratio of Total Dots to Plot",
                  choices = list("5%"=0.05,"10%"=0.1,"20%"=0.2,"50%"=0.5,"100%"=1),selected = 1),
-     sliderInput("alpha", label = h3("Transparency"), min = 0, 
+     sliderInput("alpha", label = h4("Transparency"), min = 0, 
                  max = 1, value = 1),
      checkboxGroupInput("choices", label = h4("Choose PCA Vectors"), 
                         choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3,'Choice 4'=4),
-                        selected = c(1,2))
+                        selected = c(1,2)),
+     textInput("plot_title", label = h4("Enter the plot title"), value = "Output Biplot")
      ),
+   
 
    
-   mainPanel(h1("Output Biplot",align='center'),
+   mainPanel(h3("Output Biplot",align='center'),
              plotOutput("myggbiplot"),
+             uiOutput("features"),
              uiOutput("cols"),
              helpText("Please notice the default data is generated on dataset \"wine\"  from HDclassif library,
                       The purpose of the default biplot is only for demonstration.")
